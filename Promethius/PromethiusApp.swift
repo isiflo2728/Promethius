f15bf4e33e677db465947260e2686b61c55066d9
isiflo2728 <floresisidoro30@gmail.com>
2026-07-03 15:11:14 -0700

    Initial Commit

---
+++ Promethius/PromethiusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromethiusApp.swift
+//  Promethius
+//
+//  Created by Isidoro Flores on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct PromethiusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PromethiusTests/PromethiusTests.swift
@@ -0,0 +1,19 @@
+//
+//  PromethiusTests.swift
+//  PromethiusTests
+//
+//  Created by Isidoro Flores on 7/3/26.
+//
+
+import Testing
+@testable import Promethius
+
+struct PromethiusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
